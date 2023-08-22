906125af7168b3b9798a5010d78ceff2d564d2cd
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-08-22 21:37:14 +0300

    Add files via upload

---
+++ JavaScript/parte1.js
@@ -0,0 +1,10 @@
+let userName = prompt("Как тебя зовут?");
+alert(userName);
+
+alert (Boolean(1)) //true
+alert (Boolean()) // false
+alert (Boolean("")) //false
+
+alert (number(" 123 "))
+alert (number(true))
+alert (number(false))
