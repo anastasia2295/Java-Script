b67faf68dfa13f7c29785c9415f12583e5edd370
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-08-23 19:03:23 +0300

    if ?

--- parte1.js
+++ parte1.js
@@ -1,25 +1,68 @@
-let userName = prompt("Как тебя зовут?");
-alert(userName);
+//let userName = prompt("Как тебя зовут?");
+//alert(userName);
 
-alert (Boolean(1)) //true
-alert (Boolean()) // false
-alert (Boolean("")) //false
+//alert (Boolean(1)) //true
+//alert (Boolean()) // false
+//alert (Boolean("")) //false
 
 //alert (number(" 123 "))
 //alert (number(true))
 //alert (number(false))
 
 
-let age = prompt("Возраст?" )
+//let age = prompt("Возраст?" )
 
-let message = (age<3) ? ("Здравствуй малыш"):
-(age<18) ? "Привет":
-(age<100) ? "Здравствуйте":
-"Какой необычный возраст";
+//let message = (age<3) ? ("Здравствуй малыш"):
+//(age<18) ? "Привет":
+//(age<100) ? "Здравствуйте":
+//"Какой необычный возраст";
 
-alert(message)
+//alert(message)
 
 
-let company = prompt("Какая компания создала ДжаваСкрипт?")
-(company = "Netscape") ?
-alert("Верно") : alert("Неправильно")
+
+let nameOfTheCompany = prompt("Какое официальное назване Джава скрипт?");
+if (nameOfTheCompany == "ECMAScript"){
+    alert("Верно!")
+} else {
+   alert("Не знаете? ECMAScript") 
+}
+
+let userNumber = prompt("Введите число");
+if(userNumber>0){
+    alert("1, число больше 0")
+}else if(userNumber<0){
+    alert("-1, число меньше 0")
+}else{
+   alert("0") 
+}
+
+let result
+if(a+b<4){
+    result = "Мало"
+}else {
+    result("Много")
+}
+
+let result2
+result = (a+b<4) ? 
+     "Мало" : "Много"
+
+
+
+let message
+if(login== "Сотрудник"){
+    message = "Привет"
+}else if (login == "Директор"){
+    message = "Привет"
+}else if(login=""){
+    message="Нет логина"
+}else {
+    message=""
+}
+
+
+let message2 = (login == "Сотрудник") ? "Привет":
+(login == "Директор") ? "Привет":
+(login="") ? "Нет логина":
+"";
