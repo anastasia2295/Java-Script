d9ed4e4349f5e39021ff64eb78797f64f9df08d6
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-08-24 21:12:37 +0300

    switch

---
+++ parte2.js
@@ -0,0 +1,10 @@
+if (browers == "Edge") {
+    alert("Your have got the Edge")
+}else if(browers== "Chrome" ||
+browers == "Firefox" ||
+browers == "Safari" ||
+browers == "Opera" ){
+alert("Окей,вы используйте поддерживаемый браузер")
+}else {
+    alert("Мы надеемся эта страница выглядит хорошо")
+}
