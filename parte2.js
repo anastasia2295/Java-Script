ab1ee869b6b1128ca3b384b5074832924739d5f0
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-08-25 13:44:10 +0300

    function 2

--- parte2.js
+++ parte2.js
@@ -1,3 +1,36 @@
+function pow (x, n){
+    x = prompt("Введите число 1")
+    n = prompt("Введите число 2")
+    if(n>=1) {
+        alert (x**n) 
+    }else {
+    alert("ОШибка")
+    }
+} 
+pow()
+
+
+
+function pow(x, n){
+    let result = x
+    for (i=1; i<n;i++){
+    result *=x
+}
+return(result)
+}
+let x = prompt("Введите число1")
+let n = prompt("Введите число2")
+if (n>=1 && n%1==0){
+    alert(pow(x, n))
+}else{
+    alert("Ошибка")
+}
+
+
+
+
+
+
 function checkage (age){
     return (age>18) ? true : confirm("Родители разрешили?");
 }
@@ -21,7 +54,7 @@ function min2 (number1, number2){
 }
 
 
-if (browers == "Edge") {
+if (browers = "Edge") {
     alert("Your have got the Edge")
 }else if(browers== "Chrome" ||
 browers == "Firefox" ||
