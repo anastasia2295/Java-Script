9389c9f0f977c3a660669745eec2de2f376e210b
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-08-27 16:08:21 +0300

    let const ${}

---
+++ app.js
@@ -0,0 +1,6 @@
+const defaltResulit = 0
+let currentResult = defaltResulit
+
+currentResult = (currentResult+10) * 3 / 2 - 1 
+let calculationDescription = `(${defaltResulit} + 10) * 3 / 2 - 1`
+outputResult(currentResult, calculationDescription)
