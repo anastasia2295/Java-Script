51a48a84aebbc05ed4c754fa20142d09f3768b47
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-08-27 16:15:29 +0300

    \n

--- app.js
+++ app.js
@@ -3,4 +3,6 @@ let currentResult = defaltResulit
 
 currentResult = (currentResult+10) * 3 / 2 - 1 
 let calculationDescription = `(${defaltResulit} + 10) * 3 / 2 - 1`
+let errorMessage = "An error \n" +
+"occurred!"
 outputResult(currentResult, calculationDescription)
