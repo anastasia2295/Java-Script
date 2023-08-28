f604262000c373ef2d3b62c0748390de144f0d94
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-08-28 17:30:20 +0300

    ejercicios para los funciones

--- app.js
+++ app.js
@@ -12,3 +12,17 @@ let calculationDescription = `(${defaltResulit} + 10) * 3 / 2 - 1`
 let errorMessage = "An error \n" +
 "occurred!"
 outputResult(currentResult, calculationDescription)
+
+
+function subtract (num1, num2){
+    const result = num1-num2
+    return result
+}
+alert (subtract(5, 3))
+
+function stringify (num1){
+    result = "Result" + num1
+    return result
+}
+alert(stringify(10))
+
