b99b0488a2763764b64bf40fca5c3ad9ca51a17d
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-08-28 17:24:10 +0300

    function

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 const defaltResulit = 0
 let currentResult = defaltResulit
 
-currentResult = (currentResult+10) * 3 / 2 - 1 
+function add(num1, num2){
+    const result = num1+num2
+    return result
+}
+const additionResult = add(1, 2)
+
+currentResult = additionResult
 let calculationDescription = `(${defaltResulit} + 10) * 3 / 2 - 1`
 let errorMessage = "An error \n" +
 "occurred!"
