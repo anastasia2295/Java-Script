66b7053ad8cc2b09ea0fb978515c7f147af566e2
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-08-28 19:21:06 +0300

    разделение кода на функции

--- JS_the_complete_guide_2023/app.js
+++ JS_the_complete_guide_2023/app.js
@@ -1,9 +1,15 @@
 const defaltResulit = 0
 let currentResult = defaltResulit
 
+function getUserNumberInput (){
+    return parseInt(usrInput.value)
+}
+
 function add(){
-    currentResult =  currentResult + parseInt(userInput.value)
-    outputResult(currentResult, "")
+    const enteredNumber = getUserNumberInput()
+    const calcDescription = `${currentResult} + ${enteredNumber}`
+    currentResult =  currentResult + enteredNumber
+    outputResult(currentResult, calcDescription)
 }
 
 addBtn.addEventListener("click", add)
