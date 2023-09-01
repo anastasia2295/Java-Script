74547bfb210ea2170f4b83ef93de7356ef8bf217
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-09-01 09:49:17 +0300

    operation if

--- JS_the_complete_guide_2023/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ JS_the_complete_guide_2023/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,4 +1,5 @@
-let ATTACK_VALUE = 10
+const ATTACK_VALUE = 10
+const MONSTER_ATTACK_VALUE = 14
 
 let chosenMaxLife = 100
 let currentMonsterHealth = chosenMaxLife
@@ -9,6 +10,16 @@ adjustHealthBars(chosenMaxLife)
 function attackHandler(){
     const demage = dealMonsterDamage(ATTACK_VALUE)
     currentMonsterHealth -= demage
+    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
+    currenPlayerHealth -= playerDamage
+    if (currentMonsterHealth <=0 && currenPlayerHealth>0){
+        alert("You win!")
+    }else if(currenPlayerHealth <=0 && currentMonsterHealth>0){
+        alert("You lost")
+    }else if(currenPlayerHealth <= 0 && currentMonsterHealth <= 0){
+        alert("You have a draw")
+    }
+
 }
 
 attackBtn.addEventListener("click", attackHandler)
