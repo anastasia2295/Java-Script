7f88b2da153208d0dd368f50bb4155f56494227f
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-09-01 10:13:14 +0300

    action strong attack
    action strong attack, creating a common function for normal and strong attack

--- JS_the_complete_guide_2023/control-03-monster-killer-starting-project/assets/scripts/app.js
+++ JS_the_complete_guide_2023/control-03-monster-killer-starting-project/assets/scripts/app.js
@@ -1,4 +1,5 @@
 const ATTACK_VALUE = 10
+const STRONG_ATTACK_VALUE = 17
 const MONSTER_ATTACK_VALUE = 14
 
 let chosenMaxLife = 100
@@ -7,19 +8,33 @@ let currenPlayerHealth = chosenMaxLife
 
 adjustHealthBars(chosenMaxLife)
 
-function attackHandler(){
-    const demage = dealMonsterDamage(ATTACK_VALUE)
-    currentMonsterHealth -= demage
-    const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
-    currenPlayerHealth -= playerDamage
-    if (currentMonsterHealth <=0 && currenPlayerHealth>0){
-        alert("You win!")
-    }else if(currenPlayerHealth <=0 && currentMonsterHealth>0){
-        alert("You lost")
-    }else if(currenPlayerHealth <= 0 && currentMonsterHealth <= 0){
-        alert("You have a draw")
+function attackMonster (mode){
+    let maxDamage
+    if(mode === "ATTACK"){
+        maxDamage = ATTACK_VALUE
+    }else if (mode === "STRONG_ATTACK"){
+        maxDamage = STRONG_ATTACK_VALUE
+    }
+        const demage = dealMonsterDamage(maxDamage)
+        currentMonsterHealth -= demage
+        const playerDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
+        currenPlayerHealth -= playerDamage
+        if (currentMonsterHealth <=0 && currenPlayerHealth>0){
+            alert("You win!")
+        }else if(currenPlayerHealth <=0 && currentMonsterHealth>0){
+            alert("You lost")
+        }else if(currenPlayerHealth <= 0 && currentMonsterHealth <= 0){
+            alert("You have a draw")
+        }
     }
 
+function attackHandler(){
+    attackMonster("ATTACK")
+}
+
+function strongAttackHandler () {
+    attackMonster("STRONG_ATTACK")
 }
 
 attackBtn.addEventListener("click", attackHandler)
+strongAttackBtn.addEventListener("click", strongAttackHandler)
