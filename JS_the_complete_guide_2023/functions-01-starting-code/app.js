32fc7c827bbb3e3134f1f560c26f5c9e30b094ef
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-09-07 09:08:36 +0300

    method
    Методы это функции, хранящиеся как свойства объекта

--- JS_the_complete_guide_2023/functions-01-starting-code/app.js
+++ JS_the_complete_guide_2023/functions-01-starting-code/app.js
@@ -1,1 +1,16 @@
 const startGameBtn = document.getElementById('start-game-btn');
+
+
+function startGame(){
+    console.log("Game is starting")
+
+}
+
+const person = {
+    name:"Max",
+    greet: function greet (){
+        console.log("Hello there")
+    }
+}
+person.greet() // method
+startGameBtn.addEventListener("click", startGame)
