2d48d77687b87f38dc212dcf53fde95f3e314a45
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-09-07 13:40:01 +0300

    PAPEL ROCK SCISSORS
    . Реализация основной игровой логики.

--- JS_the_complete_guide_2023/functions-01-starting-code/app.js
+++ JS_the_complete_guide_2023/functions-01-starting-code/app.js
@@ -4,6 +4,9 @@ const ROCK = "ROCK"
 const PAPER = "PAPER"
 const SCISSORS = "SCISSORS"
 const DEFAULT_USER_CHOICE = ROCK
+const RESULT_DRAW = "DRAW"
+const RESULT_PLAYER_WINS = "PLAYER_WINS"
+const RESULT_COMPUTER_WINS = "COMPUER_WINS"
 
 let gameIsRunning = false
 
@@ -20,6 +23,30 @@ const getPlayerChoice = function(){
     return selection
 }
 
+const getComputerChoice = function (){
+    const randomValue = Math.random()
+    if (randomValue < 0.34){
+    return ROCK
+}else if (randomValue < 0.67){
+    return PAPER
+}else {
+    return SCISSORS
+}
+}
+
+const getWinner = function(pChoice, cChoise){
+    if (pChoice === cChoise){
+        return RESULT_DRAW
+    } else if(
+        cChoise === ROCK && pChoice === PAPER || 
+        cChoise === PAPER && pChoice === SCISSORS ||
+        cChoise === SCISSORS && pChoice === ROCK ) {  
+return RESULT_PLAYER_WINS
+        } else {
+            return RESULT_COMPUTER_WINS
+        }
+}
+
 startGameBtn.addEventListener("click", function (){
     if (gameIsRunning){
         return
@@ -27,5 +54,7 @@ startGameBtn.addEventListener("click", function (){
     gameIsRunning = true
     console.log("Game is starting...")
     const playerSelection = getPlayerChoice()
-    console.log(playerSelection)
+    const computerChoice = getComputerChoice()
+    const winner = getWinner (computerChoice, playerSelection)
+    console.log(winner)
 })
