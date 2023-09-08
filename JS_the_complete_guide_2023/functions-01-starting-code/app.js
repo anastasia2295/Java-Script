1b1b0a11452a71a21e81cfc23c98a81f77621aa7
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-09-08 09:46:57 +0300

    Callback Functions

--- JS_the_complete_guide_2023/functions-01-starting-code/app.js
+++ JS_the_complete_guide_2023/functions-01-starting-code/app.js
@@ -78,20 +78,28 @@ startGameBtn.addEventListener("click",  () => {
 })
 
 
-const sumUp = (...numbers) => {
+const sumUp = (resultHandler, ...numbers) => {
+    const validateNumber = (number) => {
+        return isNaN(number) ? 0: number;
+    };
 let sum = 0;
 for (const num of numbers){
-sum += num
-} return sum
+sum += validateNumber(num)
+} 
+resultHandler(sum)
 }
 
 const subtractUp = function() {
     let sum = 0;
     for (const num of arguments){
-    sum += num
+    sum -= num
     } return sum
     }
 
-console.log(sumUp(1, 5, 10, -3, 6, 10))
-console.log(sumUp(1, 5, 10, -3, 6, 10, 25, 88))
+const showResult = (result) => {
+alert("The result after adding all" + result)
+}
+
+sumUp(showResult, 5, 10, -3, 6, 10)
+sumUp(showResult, 5, 10, -3, 6, 10, 25, 88)
 console.log(subtractUp(1, 10, 15, 20))
