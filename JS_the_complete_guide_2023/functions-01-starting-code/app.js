0adfe3a3211d9a9192a05705f93a9a1feeff4cc6
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-09-08 09:30:10 +0300

    ... and arguments

--- JS_the_complete_guide_2023/functions-01-starting-code/app.js
+++ JS_the_complete_guide_2023/functions-01-starting-code/app.js
@@ -78,11 +78,20 @@ startGameBtn.addEventListener("click",  () => {
 })
 
 
-const sumUp = (numbers) => {
+const sumUp = (...numbers) => {
 let sum = 0;
 for (const num of numbers){
 sum += num
 } return sum
 }
-sumUp(1, 5, 10, -3, 6, 10)
-sumUp(1, 5, 10, -3, 6, 10, 25, 88)
+
+const subtractUp = function() {
+    let sum = 0;
+    for (const num of arguments){
+    sum += num
+    } return sum
+    }
+
+console.log(sumUp(1, 5, 10, -3, 6, 10))
+console.log(sumUp(1, 5, 10, -3, 6, 10, 25, 88))
+console.log(subtractUp(1, 10, 15, 20))
