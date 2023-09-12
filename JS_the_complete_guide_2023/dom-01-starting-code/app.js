cb06b567b47284034af4f636822da800a2899b92
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-09-12 19:57:25 +0300

    доступ к title image y rating

--- JS_the_complete_guide_2023/dom-01-starting-code/app.js
+++ JS_the_complete_guide_2023/dom-01-starting-code/app.js
@@ -1,15 +1,44 @@
-const section = document.querySelector("section")
-const button = document.querySelector("button")
+const startAddMovieButton = document.querySelector("header button")
+const addMovieModal = document.getElementById("add-modal")
+const backdrop = document.getElementById ("backdrop")
+const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive")
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling
+const userInputs = addMovieModal.querySelectorAll("input")
 
-//section.style.backgroundColor = "blue"
-section.className = "reb-bg"
 
-button.addEventListener("click", () =>{
-    // if(section.className === "red-bg visible"){
-    //     section.className = "red-bg invisible"
-    // }else {
-    //     section.className = "red-bg visible"
-    // }
-    //section.classList.toggle("visible")
-    section.classList.toggle("invisible")
-})
+const toggleMovieModal = () => {
+        addMovieModal.classList.toggle("visible")
+        toggleBackdrop()
+}
+
+const toggleBackdrop = () => {
+backdrop.classList.toggle("visible")
+}
+
+const cancelAddMovie = () => {
+    toggleMovieModal()
+}
+
+const backdropClickHandler = () =>{
+    toggleMovieModal()
+}
+
+const addMovieHandler = ()=>{
+const titleValue = userInputs[0].value
+const imageUrlValue = userInputs[2].value
+const ratingValue = userInputs[3].value
+if(titleValue.trim() === "" || 
+imageUrlValue.trim()=== "" || 
+ratingValue.trim() ==="" ||
++ratingValue < 1 ||
++ratingValue > 5){
+    alert("Please, enter valid values")
+}return
+}
+
+
+startAddMovieButton.addEventListener("click", toggleMovieModal) 
+backdrop.addEventListener("click", toggleMovieModal) 
+cancelAddMovieButton.addEventListener("click", cancelAddMovie) 
+confirmAddMovieButton.addEventListener("click", addMovieHandler)
+
