262786d547b7aa7d849c88267c61524b562d5a92
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-09-12 18:39:33 +0300

    mi solition cancel y backdrop

--- JS_the_complete_guide_2023/dom-01-starting-code/dom-06-demo-project-setup/app.js
+++ JS_the_complete_guide_2023/dom-01-starting-code/dom-06-demo-project-setup/app.js
@@ -1,9 +1,23 @@
-const startAddMovieButton = document.getElementById("header button")
+const startAddMovieButton = document.querySelector("header button")
 const addMovieModal = document.getElementById("add-modal")
+const addMovieModalBackground = document.getElementById ("backdrop")
+const cancelButton = document.querySelector(".btn")
+console.log(cancelButton)
 
 const toggleMovieModal = () => {
         addMovieModal.classList.toggle("visible")
 }
 
+const changeBackground = () => {
+addMovieModalBackground.classList.toggle("visible")
+}
+
+const cancelModal = () =>{
+    addMovieModalBackground.classList.remove("visible")
+    addMovieModal.classList.remove("visible")
+}
+
 startAddMovieButton.addEventListener("click", toggleMovieModal)
-    
+startAddMovieButton.addEventListener("click", changeBackground)
+cancelButton.addEventListener("click", cancelModal)    
+
