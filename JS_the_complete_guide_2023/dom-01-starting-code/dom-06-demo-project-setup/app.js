b4606e416754ff84b07b76b458073a6d29f54a49
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-09-12 20:11:20 +0300

    добавления обьекта с названием, img и рейтингом

--- JS_the_complete_guide_2023/dom-01-starting-code/dom-06-demo-project-setup/app.js
+++ JS_the_complete_guide_2023/dom-01-starting-code/dom-06-demo-project-setup/app.js
@@ -2,11 +2,20 @@ const startAddMovieButton = document.querySelector("header button")
 const addMovieModal = document.getElementById("add-modal")
 const backdrop = document.getElementById ("backdrop")
 const cancelAddMovieButton = addMovieModal.querySelector(".btn--passive")
+const confirmAddMovieButton = cancelAddMovieButton.nextElementSibling
+const userInputs = addMovieModal.querySelectorAll("input")
 
+const movies = []
 
 const toggleMovieModal = () => {
         addMovieModal.classList.toggle("visible")
         toggleBackdrop()
+}
+
+clearMovieInput = () =>{
+    for (const userInput of userInputs){
+        userInput.value =""
+    }
 }
 
 const toggleBackdrop = () => {
@@ -15,14 +24,41 @@ backdrop.classList.toggle("visible")
 
 const cancelAddMovie = () => {
     toggleMovieModal()
+    clearMovieInput()
 }
 
 const backdropClickHandler = () =>{
     toggleMovieModal()
 }
 
+const addMovieHandler = ()=>{
+const titleValue = userInputs[0].value
+const imageUrlValue = userInputs[1].value
+const ratingValue = userInputs[2].value
+if(titleValue.trim() === "" || 
+imageUrlValue.trim()=== "" || 
+ratingValue.trim() ==="" ||
++ratingValue < 1 ||
++ratingValue > 5){
+    alert("Please, enter valid values");
+return;
+} 
+    const newMovie = {
+    title: titleValue,
+    image: imageUrlValue,
+    rating: ratingValue
+}
+movies.push(newMovie)
+console.log(movies)
+toggleMovieModal()
+clearMovieInput()
+}
+
+
+
 
 startAddMovieButton.addEventListener("click", toggleMovieModal) 
 backdrop.addEventListener("click", toggleMovieModal) 
 cancelAddMovieButton.addEventListener("click", cancelAddMovie) 
+confirmAddMovieButton.addEventListener("click", addMovieHandler)
 
