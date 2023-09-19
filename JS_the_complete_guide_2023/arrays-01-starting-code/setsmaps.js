5c714b0ce4ddf32fd4870edba631de5972189627
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-09-19 21:50:30 +0300

    sets

---
+++ JS_the_complete_guide_2023/arrays-01-starting-code/setsmaps.js
@@ -0,0 +1,10 @@
+//sets
+// const ids = new Set(['Hi', 'from', 'set!']);
+// ids.add(2);
+// if (ids.has('Hi')) {
+//   ids.delete('Hi');
+// }
+
+// for (const entry of ids.entries()) {
+//   console.log(entry[0]);
+// }
