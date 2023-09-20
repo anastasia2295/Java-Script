bb9cff9a2fb0a156a5375032af68e0e8de72be76
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-09-20 21:52:27 +0300

    newMap()
    коллекция ключ значение,где keyможет быть что угодно

--- JS_the_complete_guide_2023/arrays-01-starting-code/setsmaps.js
+++ JS_the_complete_guide_2023/arrays-01-starting-code/setsmaps.js
@@ -1,4 +1,3 @@
-//sets
 // const ids = new Set(['Hi', 'from', 'set!']);
 // ids.add(2);
 // if (ids.has('Hi')) {
@@ -8,3 +7,27 @@
 // for (const entry of ids.entries()) {
 //   console.log(entry[0]);
 // }
+
+const person1 = { name: 'Max' };
+const person2 = { name: 'Manuel' };
+
+const personData = new Map([[person1, [{ date: 'yesterday', price: 10 }]]]);
+
+personData.set(person2, [{ date: 'two weeks ago', price: 100 }]);
+
+console.log(personData);
+console.log(personData.get(person1));
+
+for (const [key, value] of personData.entries()) {
+  console.log(key, value);
+}
+
+for (const key of personData.keys()) {
+  console.log(key);
+}
+
+for (const value of personData.values()) {
+  console.log(value);
+}
+
+console.log(personData.size);
