2e9cc186ede3649ed98c311225d2a1b07adf0747
anastasia2295 <139694516+anastasia2295@users.noreply.github.com>
2023-09-21 22:19:24 +0300

    добавление деструктуризации обьекта

--- JS_the_complete_guide_2023/objects-01-starting-project/assets/scripts/objects.js
+++ JS_the_complete_guide_2023/objects-01-starting-project/assets/scripts/objects.js
@@ -1,62 +1,68 @@
-const addMovieBtn = document.getElementById("add-movie-btn")
-const serachBtn = document.getElementById("search-btn")
+const addMovieBtn = document.getElementById('add-movie-btn');
+const searchBtn = document.getElementById('search-btn');
 
-const movies = []
+const movies = [];
 
-const renderMovies = (filter = "") => {
-    const movieList = document.getElementById("movie-list")
-    if(movies.lenght === 0) {
-        movieList.classList.remove("visible")
-        return
-    }else {
-        movieList.classList.add("visible")
-        }
-        movieList.innerHTML= "";
+const renderMovies = (filter = '') => {
+  const movieList = document.getElementById('movie-list');
 
-       const filteredMovies = !filter ? movies : movies.filter(movie => movie.info.title.includes(filter))
+  if (movies.length === 0) {
+    movieList.classList.remove('visible');
+    return;
+  } else {
+    movieList.classList.add('visible');
+  }
+  movieList.innerHTML = '';
 
-        filteredMovies.forEach ((movie) => {
-     const movieEl = document.createElement("li")
-     let text = movie.info.title + "-"
-     for (const key in movie.info) {
-        if(key !== "title") {
-            text = text + `${key}: ${movie.info[key]}`;
-        }
-     }
-     movieEl.textContent = text
-     movieList.append(movieEl)
-        })
-}
+  const filteredMovies = !filter
+    ? movies
+    : movies.filter(movie => movie.info.title.includes(filter));
+
+  filteredMovies.forEach(movie => {
+    const movieEl = document.createElement('li');
+    const { info, ...otherProps } = movie;
+    console.log(otherProps);
+    const { title: movieTitle } = info;
+    let text = movieTitle + ' - ';
+    for (const key in info) {
+      if (key !== 'title') {
+        text = text + `${key}: ${info[key]}`;
+      }
+    }
+    movieEl.textContent = text;
+    movieList.append(movieEl);
+  });
+};
 
 const addMovieHandler = () => {
-    const title = document.getElementById("title").value
-    const extraName = document.getElementById("extra-name").value
-    const extraValue = document.getElementById("extra-value").value
+  const title = document.getElementById('title').value;
+  const extraName = document.getElementById('extra-name').value;
+  const extraValue = document.getElementById('extra-value').value;
 
-    if(
-        title.trim() === "" ||
-        extraName.trim() === "" ||
-        extraValue.trim() === ""  
-    ) {
-        return
-    }
+  if (
+    title.trim() === '' ||
+    extraName.trim() === '' ||
+    extraValue.trim() === ''
+  ) {
+    return;
+  }
 
+  const newMovie = {
+    info: {
+      title,
+      [extraName]: extraValue
+    },
+    id: Math.random().toString()
+  };
 
-    const newMovie = {
-        info: {
-            title,
-            [extraName]: extraValue
-        },
-        id: Math.random()
-    }
-    movies.push(newMovie)
-    renderMovies()
-}
+  movies.push(newMovie);
+  renderMovies();
+};
 
 const searchMovieHandler = () => {
-    const filterTerm = document.getElementById("filter-title").value
-    renderMovies(filterTerm)
-}
+  const filterTerm = document.getElementById('filter-title').value;
+  renderMovies(filterTerm);
+};
 
-addMovieBtn.addEventListener("click", addMovieHandler)
-serachBtn.addEventListener("click", searchMovieHandler)
+addMovieBtn.addEventListener('click', addMovieHandler);
+searchBtn.addEventListener('click', searchMovieHandler);
